b8f05ee120a4d4be1abb55169c7806b8c6ad1d3e
KanchanSarkar <sarkar.kanchan@gmail.com>
2023-03-06 21:16:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+document.querySelector('.check1').addEventListener('click', function () {
+    console.log("Mobile-1");
+});
+document.querySelector('.check2').addEventListener('click', function () {
+    console.log("Mobile-2");
+});
+document.querySelector('.check3').addEventListener('click', function () {
+    console.log("Mobile-3");
+});
+
+
